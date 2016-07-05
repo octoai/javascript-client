b986729f36faa255bfe82fd11cc43bf4927e612c
Pranav Prakash <pranny@gmail.com>
2016-07-05 10:34:56 +0530

    Updating octo.js
    Summary: Updating octo.js to find categories and tags from the page itself
    
    Test Plan: no test plan
    
    Reviewers: ravingupta, praateek.mahajan, shivin.agarwal
    
    Reviewed By: shivin.agarwal
    
    Subscribers: dipankar
    
    Differential Revision: https://phab.octo.ai/D155

--- libs/octo.js
+++ libs/octo.js
@@ -1,19 +1,116 @@
 (function($) {
-  
+
   $.octo = {};
+
+  $.octo.apikey = 'OVERRIDE THIS WITH YOUR API KEY';
+
+  $.octo.defaults = {
+    base_url : 'https://api.octo.ai',
+    cookie_name: '_oid'
+  };
+
+  function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  }
+
+
+  // the pageview event call
+  $.octo.pageView = function(options) {
+    if (options == undefined) {
+      options = {};
+    }
+    var _tags = $.octo.unique($.merge($.octo.pageTags(), options.tags || []));
+    var _cats = $.octo.unique($.merge($.octo.pageCategories(), options.categories || []));
+
+    var pageViewOpts = {
+      userId: options.userId || -1,
+      browserDetails: $.octo.browserDetails(options.cookie_name),
+      routeUrl: window.location.pathname,
+      tags: _tags,
+      categories: _cats
+    };
+    var pageUrl = $.octo.defaults.base_url.concat("/events/page.view/");
+    $.octo.ajax(pageUrl, pageViewOpts);
+  };
+
+  // gets the cookie id corresponding to a cookie name
+  $.octo.getCookieId = function(cookie_name) {
+    var _cname = cookie_name || $.octo.defaults.cookie_name;
+    if($.cookie(_cname)) {
+      return $.cookie(_cname);
+    }
+    else {
+      var _oid = guid();
+      $.cookie(_cname, _oid);
+      return _oid;
+    }
+  };
+
+  // get opengraph tags from the page
+  $.octo.OGTags = function() {
+    var tags = [];
+    $.each($("meta[property='article:tag']"), function(i, tag) {
+      tags.push(tag.content);
+    });
+    return tags;
+  };
+
+  //get opengraph section from the page
+  $.octo.OGSection = function(){
+    return $("meta[property='article:section']").attr('content');
+  };
+
+  // get standard HTTP META keywords
+  $.octo.metaKeywords = function(){
+    var kwds = $("meta[name=keywords]").attr('content');
+    if (kwds == undefined){
+      return [];
+    } else {
+      return kwds.split(',');
+    }
+  };
+
+  // get combined tags
+  $.octo.pageTags = function() {
+    return $.merge($.octo.OGTags(), $.octo.metaKeywords());
+  };
+
+  // get page categories
+  $.octo.pageCategories = function() {
+    var cats = [];
+    if( $.octo.OGSection())
+      cats.push($.octo.OGSection());
+    return cats;
+  };
+
+  // unique
+  $.octo.unique = function(list) {
+    var result = [];
+    $.each(list, function(i, e) {
+        if ($.inArray(e, result) == -1) result.push(e);
+    });
+    return result;
+  };
+
 
   $.octo.ajax = function(url, data, options) {
     $.octo.ajax.settings = $.extend({}, $.octo.ajax.defaults, options);
     var opts = {
-      error: onError,
-      success: onSuccess,
+      error: $.octo.ajax.settings.onError,
+      success: $.octo.ajax.settings.onSuccess,
       data: JSON.stringify(data),
       contentType: $.octo.ajax.settings.contentType,
       accepts: $.octo.ajax.settings.accepts,
       dataType: $.octo.ajax.settings.dataType,
       method: $.octo.ajax.settings.method,
       headers: {
-        apikey: $.octo.ajax.settings.apikey
+        apikey: $.octo.apikey
       }
     };
     $.ajax(url, opts);
@@ -23,7 +120,15 @@
     contentType: 'application/json',
     accepts: 'text/html',
     dataType: 'json',
-    method: 'POST'
+    method: 'POST',
+    onError: $.octo.onAjaxError,
+    onSuccess: $.octo.onAjaxSuccess
+  };
+
+  $.octo.onAjaxSuccess = function(data) {
+  };
+
+  $.octo.onAjaxError = function(data) {
   };
 
   $.octo.browser_name = function(){
@@ -52,6 +157,27 @@
     } else if (isChrome) {
       return "Chrome";
     } else {
+      return "unknown";
+    }
+  };
+
+
+  $.octo.browserDetails = function(cookie_name){
+    return {
+      name: $.octo.browser_name(),
+      platform: navigator.platform,
+      manufacturer: navigator.vendor,
+      cookieid: $.octo.getCookieId(cookie_name)
+    };
+  };
+
+
+  $.octo.init = function(options){
+    $( document ).ready(function() {
+      $.octo.pageView(options);
+    });
+  };
+
       return "unknown";
     }
   }
